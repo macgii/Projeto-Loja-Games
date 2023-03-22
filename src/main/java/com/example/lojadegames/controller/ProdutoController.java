5682938869e24c14ebf223f08456a5fb716441e0
Giovana <maiiaraoliveiras1@gmail.com>
2023-03-21 23:58:04 -0300

    Projeto - Loja de Games adicionando CrossOrigin

--- src/main/java/com/example/lojadegames/controller/ProdutoController.java
+++ src/main/java/com/example/lojadegames/controller/ProdutoController.java
@@ -8,17 +8,23 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.lojadegames.model.Produto;
 import com.example.lojadegames.repository.CategoriaRepository;
 import com.example.lojadegames.repository.ProdutoRepository;
 
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 	
 	@Autowired
